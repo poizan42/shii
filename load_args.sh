d3ecdc212028ea6ac4c1aea2eed167ac2f17becb
Kasper F. Brandt <poizan@poizan.dk>
2014-08-06 04:45:53 +0200

    better defaults for username/nick

--- load_args.sh
+++ load_args.sh
@@ -1,8 +1,8 @@
 irc_server=irc.freenode.net
 irc_port=6667
 irc_dir="$HOME/irc"
-irc_nick="$USER"
-irc_username="$USER"
+#irc_nick="$USER"
+#irc_username="$USER"
 runtime_dir_base="/tmp/shii"
 
 while getopts ":s:i:n:u:f:k:r:" opt; do
@@ -38,6 +38,15 @@ while getopts ":s:i:n:u:f:k:r:" opt; do
 	esac
 done
 
+if [ -z "$irc_username" ] && [ -z "$irc_nick" ]; then
+	irc_username="$USER"
+	irc_nick="$USER"
+elif [ -z "$irc_username" ]; then
+	irc_username="$irc_nick"
+elif [ -z "$irc_nick" ]; then
+	irc_nick="$irc_username"
+fi
+
 if [ -z "$irc_realname" ]; then
-	irc_realname="$irc_nick"
+	irc_realname="$irc_username"
 fi
